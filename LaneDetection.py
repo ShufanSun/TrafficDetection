ef2b4d0ffcdd38a874f61ce10d825bd75950f16a
ShufanSun <ssun329@wisc.edu>
2023-11-11 13:04:27 -0600

    Yolo setup commands

---
+++ LaneDetection.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().system('pip install ultralytics')
+
+
+# In[4]:
+
+
+import ultralytics
+ultralytics.checks()
+
+
+# In[6]:
+
+
+from ultralytics import YOLO
+
+
+# In[ ]:
+
+
+
+
