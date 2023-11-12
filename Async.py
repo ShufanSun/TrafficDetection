2c51fb26faf0ed2158dc91d092eddacc05ea7cf9
Benjamin G <randomblock1@pm.me>
2023-11-12 04:55:24 -0600

    fix lag

--- Async.py
+++ Async.py
@@ -1,38 +1,40 @@
 from ultralytics import YOLO
 import cv2
-from glob import glob
 from PIL import Image
 import os
+from time import sleep
 
 model = YOLO("yolov8-trained.pt")
 
 video_path = './Videos/out4.mp4'
 cap = cv2.VideoCapture(video_path)
+
 i = 0
 
 while cap.isOpened():
     ret, frame = cap.read()
     if ret:
-        cv2.imwrite(f'./Temporary/frame{i}.jpg', frame)
-        results = model([f'./Temporary/frame{i}.jpg'])
+        i += 1
+        if i % 5 != 0:
+            # remove sleep for maxumum speed
+            sleep(1/20)
+            continue
+        
+        cv2.imshow("Traffic Detection", frame)
+        cv2.waitKey(1)
+
+        # results = model(frame)
        
-        for result in results:
-            boxes = result.boxes  # Boxes object for bbox outputs
-            masks = result.masks  # Masks object for segmentation masks outputs
-            keypoints = result.keypoints  # Keypoints object for pose outputs
-            probs = result.probs  # Probs object for classification outputs
+        # for result in results:
+        #     boxes = result.boxes  # Boxes object for bbox outputs
+        #     masks = result.masks  # Masks object for segmentation masks outputs
+        #     keypoints = result.keypoints  # Keypoints object for pose outputs
+        #     probs = result.probs  # Probs object for classification outputs
     
-        for idx, r in enumerate(results):
-            im_array = r.plot()  # plot a BGR numpy array of predictions
-            image = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+        # for idx, r in enumerate(results):
+            # im_array = r.plot()  # plot a BGR numpy array of predictions
+            # image = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
             
-            # Save the processed image by overwriting the original
-            filename = f'./Temporary/frame{i}.jpg'
-            image.save(filename)
-            
-            i += 5
-            cap.set(cv2.CAP_PROP_POS_FRAMES, i)
-            print(i)
     else:
         cap.release()
         break
